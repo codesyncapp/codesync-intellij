42730bbb8bf7e9745032f350050a8127b2abc6c7
Basit-qc <basit.qc@gmail.com>
2021-01-09 05:40:29 +0500

    End-to-End completed (git branch on runtime is left)

--- src/main/java/org/intellij/sdk/codesync/ProjectOpenCloseListener.java
+++ src/main/java/org/intellij/sdk/codesync/ProjectOpenCloseListener.java
@@ -12,19 +12,16 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vfs.VirtualFile;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.LinkedList;
-
 import name.fraser.neil.plaintext.diff_match_patch;
 import name.fraser.neil.plaintext.diff_match_patch.Patch;
-
-//import name.fraser.neil.plaintext.StandardBreakScorer;
-//import name.fraser.neil.plaintext.diff_match_patch;
 import org.jetbrains.annotations.NotNull;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Listener to detect project open and close.
@@ -38,6 +35,8 @@ public class ProjectOpenCloseListener implements ProjectManagerListener {
    */
 
   String CODESYNC_ROOT = "/usr/local/bin/.codesync";
+  String DIFFS_REPO = String.format("%s/.diffs", CODESYNC_ROOT);
+  String MAGIC_STRING = "IntellijIdeaRulezzz";
 
   @Override
   public void projectOpened(@NotNull Project project) {
@@ -53,9 +52,7 @@ public class ProjectOpenCloseListener implements ProjectManagerListener {
         VirtualFile file = FileDocumentManager.getInstance().getFile(document);
         String file_path = file.getPath();
         String currentText = document.getText();
-        currentText = currentText.replace("IntellijIdeaRulezzz ", "").trim();
-
-//        const editor = vscode.window.activeTextEditor;
+        currentText = currentText.replace( MAGIC_STRING, "").trim();
         // TODO: Get on run time
         String branch = "plugins";
         String repoName = project.getName();
@@ -63,6 +60,9 @@ public class ProjectOpenCloseListener implements ProjectManagerListener {
         String s = String.format("%s/", repoPath);
         String[] rel_path_arr = file_path.split(s);
         String relPath = rel_path_arr[rel_path_arr.length - 1];
+        if (relPath.startsWith("/")) {
+          relPath = relPath.replace("/", "");
+        }
         String shadowPath = String.format("%s/%s/%s/%s", CODESYNC_ROOT, repoName, branch, relPath);
         File f = new File(shadowPath);
         if (!f.exists()) {
@@ -70,27 +70,39 @@ public class ProjectOpenCloseListener implements ProjectManagerListener {
           return;
         }
         // Read shadow file
-        Path shadowFilePath = Path.of(shadowPath);
-        String shadowText = "";
+        String shadowText = ReadFileToString.readLineByLineJava8(shadowPath);
         try {
-          shadowText = Files.readString(shadowFilePath);
+          FileWriter myWriter = new FileWriter(shadowPath);
+          myWriter.write(currentText);
+          myWriter.close();
         } catch (IOException e) {
           e.printStackTrace();
         }
-        // Update shadow file
-        try {
-          Files.writeString(shadowFilePath, currentText);
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-
-//        diff_match_patch dmp = new diff_match_patch(new StandardBreakScorer());
         diff_match_patch dmp = new diff_match_patch();
-        LinkedList<Patch> patches = dmp.patch_make("Hello World.", "Goodbye World.");
-        //  Create text representation of patches objects
+        LinkedList<Patch> patches = dmp.patch_make(shadowText, currentText);
+        // Create text representation of patches objects
         String diffs = dmp.patch_toText(patches);
-        System.out.println(diffs);
-//        System.out.println(currentText);
+        // Create YAML dump
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("repo", repoName);
+        data.put("branch", branch);
+        data.put("file", relPath);
+        data.put("diff", diffs);
+        final Date currentTime = new Date();
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        String created_at = sdf.format(currentTime);
+        data.put("created_at", created_at);
+        Yaml yaml = new Yaml();
+        String diffFileName = String.format("%s/%s.yml", DIFFS_REPO, System.currentTimeMillis());
+        // Write diff file
+        FileWriter writer = null;
+        try {
+          writer = new FileWriter(diffFileName);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        yaml.dump(data, writer);
       }
     }, Disposer.newDisposable());
   }
---
+++ src/main/java/org/intellij/sdk/codesync/ReadFileToString.java
@@ -0,0 +1,29 @@
+package org.intellij.sdk.codesync;
+
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ReadFileToString
+{
+    //Read file content into the string with - Files.lines(Path path, Charset cs)
+
+    public static String readLineByLineJava8(String filePath)
+    {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return contentBuilder.toString();
+    }
+}
