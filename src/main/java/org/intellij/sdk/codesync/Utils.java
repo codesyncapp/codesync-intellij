850dec77541aa8fcf6e09c5e55e27af5ff763168
Basit-qc <basit.qc@gmail.com>
2021-02-19 11:25:31 +0500

    Skipping directory events for New/Deleted events

--- src/main/java/org/intellij/sdk/codesync/Utils.java
+++ src/main/java/org/intellij/sdk/codesync/Utils.java
@@ -182,6 +182,9 @@ public class Utils {
 
     public static void FileCreateHandler(VFileEvent event, String repoName, String repoPath) {
         String filePath = event.getFile().getPath();
+        // Skip in case of directory
+        File eventFile = new File(filePath);
+        if (eventFile.isDirectory()) { return; }
         String s = String.format("%s/", repoPath);
         String[] rel_path_arr = filePath.split(s);
         String relPath = rel_path_arr[rel_path_arr.length - 1];
@@ -232,6 +235,9 @@ public class Utils {
 
     public static void FileDeleteHandler(VFileEvent event, String repoName, String repoPath) {
         String filePath = event.getFile().getPath();
+        // Skip in case of directory
+        File eventFile = new File(filePath);
+        if (eventFile.isDirectory()) { return; }
         String s = String.format("%s/", repoPath);
         String[] rel_path_arr = filePath.split(s);
         String relPath = rel_path_arr[rel_path_arr.length - 1];
