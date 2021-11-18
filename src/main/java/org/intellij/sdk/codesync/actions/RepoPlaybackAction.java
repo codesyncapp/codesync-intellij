64745204607e50ac69fe83aebaba0a8b59513689
Saleem Latif <saleem.latif.ee@gmail.com>
2021-11-18 22:12:38 +0500

    Added actions for file and repo callback.

---
+++ src/main/java/org/intellij/sdk/codesync/actions/RepoPlaybackAction.java
@@ -0,0 +1,17 @@
+package org.intellij.sdk.codesync.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class RepoPlaybackAction extends AnAction {
+    @Override
+    public void update(AnActionEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
