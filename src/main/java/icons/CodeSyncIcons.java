faad86f41e2b948c08da3895c7ac9cbf39f9ad02
Saleem Latif <saleem.latif.ee@gmail.com>
2022-09-04 18:48:45 +0500

    chore: quality fixes.

--- src/main/java/icons/CodeSyncIcons.java
+++ src/main/java/icons/CodeSyncIcons.java
@@ -5,6 +5,10 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class CodeSyncIcons {
+    private CodeSyncIcons(){
+        // restrict instantiation
+    }
+
     public static final Icon codeSyncIcon = IconLoader.getIcon("/icons/icon.png", CodeSyncIcons.class);
 
     public static Icon getCodeSyncIcon () {
