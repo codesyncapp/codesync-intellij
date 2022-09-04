a4805be5e7cc8e6080e20c51ad3ac5f7ae4b8dd4
Saleem Latif <saleem.latif.ee@gmail.com>
2022-09-04 12:52:31 +0500

    feat: Added codesync icon on user input dialogs.

---
+++ src/main/java/icons/CodeSyncIcons.java
@@ -0,0 +1,13 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class CodeSyncIcons {
+    public static final Icon codeSyncIcon = IconLoader.getIcon("/icons/icon.png", CodeSyncIcons.class);
+
+    public static Icon getCodeSyncIcon () {
+        return codeSyncIcon;
+    }
+}
--- src/main/java/icons/SdkIcons.java
+++
@@ -1,9 +0,0 @@
-package icons;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
-
-public class SdkIcons {
-    public static final Icon codeSyncIcon = IconLoader.getIcon("/icons/icon.png", SdkIcons.class);
-}
