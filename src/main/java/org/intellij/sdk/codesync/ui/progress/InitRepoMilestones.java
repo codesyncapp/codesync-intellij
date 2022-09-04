38eee368f55bc00274dc2503b0c760a0890c7403
Saleem Latif <saleem.latif.ee@gmail.com>
2022-09-04 16:06:51 +0500

    feat: Updated wording of notifications to correctly indicate repo or branch that is being synced.

--- src/main/java/org/intellij/sdk/codesync/ui/progress/InitRepoMilestones.java
+++ src/main/java/org/intellij/sdk/codesync/ui/progress/InitRepoMilestones.java
@@ -19,8 +19,8 @@ public final class InitRepoMilestones {
     public static MileStone START = new MileStone(0.00, "Sync started");
     public static MileStone CHECK_USER_ACCESS = new MileStone(0.10, "Checking access");
     public static MileStone FETCH_FILES = new MileStone(0.20, "Fetching files");
-    public static MileStone COPY_FILES = new MileStone(0.30, "Fetching files");
-    public static MileStone SENDING_REPO = new MileStone(0.40, "Sending repo to server");
+    public static MileStone COPY_FILES = new MileStone(0.30, "Copying files");
+    public static MileStone SENDING_REPO = new MileStone(0.40, "Sending file to the server");
     public static MileStone PROCESS_RESPONSE = new MileStone(0.50, "Processing server response");
     public static MileStone CONFIG_UPDATE = new MileStone(0.60, "Updating local config");
     public static MileStone UPLOAD_FILES = new MileStone(0.70, "Uploading files");
