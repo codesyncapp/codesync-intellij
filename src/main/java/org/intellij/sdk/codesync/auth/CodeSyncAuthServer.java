0407602a6feaee340a2d7d4ad08a71b4b324a594
Saleem Latif <saleem.latif.ee@gmail.com>
2022-09-07 20:39:44 +0500

    fix: Increased thread count to 10 as needed by the server.

--- src/main/java/org/intellij/sdk/codesync/auth/CodeSyncAuthServer.java
+++ src/main/java/org/intellij/sdk/codesync/auth/CodeSyncAuthServer.java
@@ -34,7 +34,7 @@ public class CodeSyncAuthServer {
     }
 
     public void start() throws Exception {
-        int maxThreads = 2;
+        int maxThreads = 10;
         int minThreads = 1;
         int idleTimeout = 120;
 
