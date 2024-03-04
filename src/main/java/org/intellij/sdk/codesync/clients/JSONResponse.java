a98b06495387c4e5429aa16b3d35f323af60efa5
saadberry <saadberry29@gmail.com>
2024-03-04 22:33:00 +0500

    Feedback Implementation

--- src/main/java/org/intellij/sdk/codesync/clients/JSONResponse.java
+++ src/main/java/org/intellij/sdk/codesync/clients/JSONResponse.java
@@ -65,7 +65,7 @@ public class JSONResponse {
     Validate the status code of the API response and throw status code exception if the response status is not 2xx.
     */
     public void raiseForStatus () throws StatusCodeError {
-        Integer errorCode;
+        Number errorCode = null;
         int customErrorCode = 0;
         // Only throw for client or server error, all other responses are considered success response.
         if(this.statusCode >= 400) {
@@ -79,8 +79,10 @@ public class JSONResponse {
                     errorMessage = (String) errorObject.get("message");
                 }
                 if (errorObject != null && errorObject.containsKey("error_code")) {
-                    errorCode = ((Number) errorObject.get("error_code")).intValue();
-                    customErrorCode = errorCode;
+                    errorCode = ((Number) errorObject.get("error_code"));
+                    if (errorCode != null){
+                        customErrorCode = errorCode.intValue();
+                    }
                 }
             }
             throw new StatusCodeError(this.statusCode, customErrorCode, errorMessage);
